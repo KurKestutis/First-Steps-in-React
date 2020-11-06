183e9c123f80765a4ea6cc9002e321d66d960980
Kestutis Kurienius <kurkestutis@gmail.com>
2020-11-06 17:59:52 +0200

    id and className added

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -2,14 +2,14 @@ import React from 'react'
 
 const Hello = () => {
     // return(
-    //     <div>
+    //     <div className: 'dummyClass'>
     //         <h1>HELLO</h1>
     //     </div>
     // )
 
     return React.createElement(
         'div',
-        null,
+        {id: 'hello', className: 'dummyClass'},
         React.createElement('h1', null, 'Component WITHOUT JSX')
     )
 }
