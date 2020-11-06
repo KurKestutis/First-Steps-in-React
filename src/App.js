bfd81ea260cb4442554fa62dac1b4f40ce4e65ff
Kestutis Kurienius <kurkestutis@gmail.com>
2020-11-06 17:39:36 +0200

    App.js changed from Function to Class

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Greet from './components/Greet'
 import Welcome from './components/Welcome'
 import Hello from './components/Hello'
 
-function App() {
-  return (
-    <div className="App">
+// function App() {
+//   return (
+//     <div className="App">
+//       <Greet/>
+//       <Welcome/>
+//       <Hello />
+//     </div>
+//   );  
+// }
+
+class App extends Component{
+  render(){ 
+    return (
+      <div className="App">
       <Greet/>
       <Welcome/>
       <Hello />
     </div>
-  );
+   );
+  }
 }
 
 export default App;
