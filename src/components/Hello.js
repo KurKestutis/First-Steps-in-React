109d969bd9d53fe036b868fbd06ddd1f0d8f641b
Kestutis Kurienius <kurkestutis@gmail.com>
2020-11-06 17:53:11 +0200

    Component WITHOUT JSX added

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,10 +1,16 @@
 import React from 'react'
 
 const Hello = () => {
-    return(
-        <div>
-            <h1>HELLO</h1>
-        </div>
+    // return(
+    //     <div>
+    //         <h1>HELLO</h1>
+    //     </div>
+    // )
+
+    return React.createElement(
+        'div',
+        null,
+        React.createElement('h1', null, 'Component WITHOUT JSX')
     )
 }
 
