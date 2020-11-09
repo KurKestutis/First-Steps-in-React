3be17d18f9867b05cd31dc39e9bc509c2e003e79
Kestutis Kurienius <kurkestutis@gmail.com>
2020-11-09 14:31:31 +0200

    setState change with increment

---
+++ src/components/Counter.js
@@ -0,0 +1,37 @@
+// class component
+import React, { Component } from 'react'
+
+class Counter extends Component {
+
+    constructor(props){
+        super(props)
+
+        this.state = {
+            count : 0
+        }
+    }
+
+    increment(){
+        this.setState({
+            count:this.state.count +1
+        },
+        () => {
+            console.log('Callback value', this.state.count)
+        } )
+        console.log('Prieš "Incrysmentą": ', this.state.count)
+    }
+
+    render() {
+        return (
+            <div>
+            <div>
+                count - {this.state.count}
+                <br></br>
+                <button onClick={() => this.increment()}>Increment</button>
+            </div>
+            </div>
+        )
+    }
+}
+
+export default Counter
