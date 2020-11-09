d87e67510aa4ea9d065f5056de120eb2c7e0fed8
Kestutis Kurienius <kurkestutis@gmail.com>
2020-11-09 15:22:44 +0200

    CSS multiple classes use template literals

--- src/components/Stylesheet.js
+++ src/components/Stylesheet.js
@@ -5,7 +5,7 @@ function Stylesheet(props) {
     let className = props.primary ? 'primary' : ''    
     return (
         <div>
-            <h1 className={className}>Stelesheets</h1>
+            <h1 className={`${className} font-xl`}>Stelesheets</h1>
         </div>
     )
 }
