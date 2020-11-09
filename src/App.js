29e986dfd67d9d41c8f954538ef5618f71fa476f
Kestutis Kurienius <kurkestutis@gmail.com>
2020-11-09 15:02:23 +0200

    CSS added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Welcome from './components/Welcome'
 import Hello from './components/Hello'
 import Message from './components/Message'
 import Counter from './components/Counter';
+import Stylesheet from './components/Stylesheet';
 
 class App extends Component{
   render(){ 
@@ -13,6 +14,7 @@ class App extends Component{
       <div className="App">
         <Counter/>
       {/* <Message/> */}
+      <Stylesheet primary={false}/>
                   
     </div>
    );
---
+++ src/components/Stylesheet.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './myStyle.css'
+
+function Stylesheet() {
+    
+    return (
+        <div>
+            <h1 className='primary'>Stelesheets</h1>
+        </div>
+    )
+}
+
+export default Stylesheet
