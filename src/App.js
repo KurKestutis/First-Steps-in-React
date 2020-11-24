c44e867b1a8849d9093ec369fe3ee1d8846f6347
Kestutis Kurienius <kurkestutis@gmail.com>
2020-11-24 13:18:54 +0200

    Cleared App.js for next exercises

--- src/App.js
+++ src/App.js
@@ -1,64 +1,16 @@
-import React, {Component} from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Greet from './components/Greet'
-import Welcome from './components/Welcome'
-import Hello from './components/Hello'
-import Message from './components/Message'
-import Counter from './components/Counter';
-import Stylesheet from './components/Stylesheet';
-import Inline from './components/Inline';
-import './appStyle.css'
-import styles from './appStyles.module.css'
-import Labas from './components/Labas'
-import Kitty from './components/Kitty'
+import React, { Component } from "react";
 
-class App extends Component{
- 
-  render(){ 
+import "./App.css";
 
-    function formatDate(date) {
-      return date.toLocaleDateString();
-    }
+import Labas from "./components/Labas";
 
-    const comment = {
-      date: new Date(),
-      text: 'I hope you enjoy learning React!',
-      author: {
-        name: 'Hello Kitty',
-        avatarUrl: 'https://placekitten.com/g/64/64',
-      },
-    };
-
+class App extends Component {
+  render() {
     return (
-
-      
-
       <div className="App">
-
-      <Message/>
-      <Labas name='Saras'/>
-
-      <Kitty  
-        date={comment.date}
-        text={comment.text}
-        author={comment.author}/>
-
-      <Greet name='Diana' heroName='Superwoman'/>
-     
-
-      <Counter/>
-      {/* <Stylesheet primary={true}/>
-
-      <Inline/>  */}
-      
-      <h2 className='error'>Error</h2>
-      <h2 className={styles.success}>Success</h2>
-      
-      <Welcome name='Jonas' heroName='Iroman'/>
-
-    </div>
-   );
+        <Labas name="Saras" />
+      </div>
+    );
   }
 }
 
